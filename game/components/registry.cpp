c2811d2916e61b5a25b4a8d9c0f823809f7e1fea
Samuel Grigolato <samuel.grigolato@gmail.com>
2018-04-03 17:06:52 -0300

    Add facility for traversing component list pairwise

--- game/components/registry.cpp
+++ game/components/registry.cpp
@@ -23,6 +23,24 @@ void for_each_component(std::function<bool(std::shared_ptr<Component>)> action) 
     }
 }
 
+void for_each_component_pair(std::function<bool(std::shared_ptr<Component>)> applies_for,
+                             std::function<void(std::shared_ptr<Component>, std::shared_ptr<Component>)> action) {
+    std::list<std::shared_ptr<Component>>::iterator it1 = components_list.begin();
+    while (it1 != components_list.end()) {
+        if (applies_for(*it1)) {
+            std::list<std::shared_ptr<Component>>::iterator it2 = it1;
+            it2++;
+            while (it2 != components_list.end()) {
+                if (applies_for(*it2)) {
+                    action(*it1, *it2);
+                }
+                it2++;
+            }
+        }
+        it1++;
+    }
+}
+
 void add_component(std::shared_ptr<Component> component) {
     component->init();
     components_list.push_back(component);
--- game/components/registry.h
+++ game/components/registry.h
@@ -11,6 +11,9 @@ namespace components {
 
 void for_each_component(std::function<bool(std::shared_ptr<Component>)> action);
 
+void for_each_component_pair(std::function<bool(std::shared_ptr<Component>)> applies_for,
+                             std::function<void(std::shared_ptr<Component>, std::shared_ptr<Component>)> action);
+
 void add_component(std::shared_ptr<components::Component> component);
 
 }}
